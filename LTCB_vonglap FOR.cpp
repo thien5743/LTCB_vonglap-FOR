9270e49ae9538a751a023f88878514f3497498fe
thien5743 <thientran5743@gmail.com>
2024-03-04 11:07:06 +0700

    Add project files.

---
+++ LTCB_vonglap FOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int sodau, socuoi, i = 0;
+	cout << "vong lap for \n";
+	cout << "nhap so dau:\n";
+	cin >> sodau;
+	cout << "nhap so cuoi:";
+	cin >> socuoi;
+	for (int i = 0; i <= sodau; i++)
+		cout << i << "\n";
+	{
+
+	}
+	return 0;
+}
